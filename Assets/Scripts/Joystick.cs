20c5eadb852a908c0fc4658b32984aeba526be23
Dejan Popov <dekipopica92@gmail.com>
2020-11-03 16:53:56 +0100

    Update Joystick.cs
    - Joystick for android

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -6,6 +6,14 @@ public class Joystick : MonoBehaviour
 {
     public Transform player;
     public float speed = 5.0f;
+
+    //Is player touching screen
+    private bool touchStart = false;
+
+    //2D - 2 Vectors needed
+    private Vector2 pointA;
+    private Vector2 pointB;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +23,12 @@ public class Joystick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Converter
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+               Input.mousePosition.y, Camera.main.transform.position.z));
+        }
     }
 
     void moveCharacter(Vector2 direction)
